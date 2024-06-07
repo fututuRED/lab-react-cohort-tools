c959fcbd03b685309e0d80b26ac60a7435ae1730
Reda Dahmane <r.dahmanee@gmail.com>
2024-06-07 15:36:24 +0200

    ok

--- src/App.jsx
+++ src/App.jsx
@@ -3,10 +3,9 @@ import Navbar from "./components/Navbar";
 import HomePage from "./pages/HomePage";
 import StudentDetailsPage from "./pages/StudentDetailsPage";
 import UserProfilePage from "./pages/UserProfilePage";
-import { Route, Routes, useParams } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 
 function App() {
-  let { studentId } = useParams();
   return (
     <div className="App relative z-20 pt-20">
       <Navbar />
